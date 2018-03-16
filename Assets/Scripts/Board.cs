8122af8f66ddcf0236ca6601c636f9c60a32aea8
hidder11 <hidder11@gmail.com>
2018-03-16 16:32:54 +0100

    Fixed build error

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using NUnit.Framework;
 using UnityEngine;
 using Random = System.Random;
 
