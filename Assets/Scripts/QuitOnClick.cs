3f23a29af87bdebe529c08fdf89ef161c866da94
Aran Kieskamp <aran@arankieskamp.me>
2018-03-18 18:33:08 +0100

    Added buildsettings fixed quit button

--- Assets/Scripts/QuitOnClick.cs
+++ Assets/Scripts/QuitOnClick.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuitOnClick : MonoBehaviour {
-
-	public void Quit()
-	{
-		
-	}
+public class QuitOnClick : MonoBehaviour
+{
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
