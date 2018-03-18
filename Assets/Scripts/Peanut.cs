98470c7e9cfb068bac1b6c5a9c6db3df75454112
hidder11 <hidder11@gmail.com>
2018-03-18 17:43:24 +0100

    small fix

--- Assets/Scripts/Peanut.cs
+++ Assets/Scripts/Peanut.cs
@@ -26,25 +26,6 @@ public class Peanut : MonoBehaviour
     }
 
     public GameObject GObject { get; set; }
-    
-    Color32 AverageColorFromTexture(Texture2D tex)
-    {
-        Color32[] texColors = tex.GetPixels32();
-        int total = texColors.Length;
- 
-        float r = 0;
-        float g = 0;
-        float b = 0;
- 
-        for(int i = 0; i < total; i++)
-        {
-            r += texColors[i].r;
-            g += texColors[i].g;
-            b += texColors[i].b;
-        }
-        return new Color32((byte)(r / total) , (byte)(g / total) , (byte)(b / total) , 0);
-    }
-
 
     private void Setup()
     {
